00627ec14115a52fcf4754a5023adb40f9ab2c62
woshizize <abc123456780321@163.com>
2026-07-02 11:26:55 +0800

    ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -15,11 +15,11 @@ public class ExampleJavaMod extends Mod{
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
+                BaseDialog dialog = new BaseDialog("awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa awa");
+                dialog.cont.add("是的我知道你不点按钮就玩不了游戏").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.cont.button("点我是gay", dialog::hide).size(1000f, 500f);
                 dialog.show();
             });
         });
